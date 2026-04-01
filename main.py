65095aaa6af85ec9d30b25f5a1c26a856f60e920
Patricia Santoro <patricia.recados@yahoo.com.br>
2026-04-01 15:45:08 -0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+def run():
+    print("=== AI Recommendation System ===\n")
 
+    category = input("Choose a category (ai, marketing, programming, business): ")
+
+    from recommender import recommend
+    results = recommend(category)
+
+    if not results:
+        print("\nNo recommendations found.")
+        return
+
+    print("\nRecommended items:\n")
+
+    for item in results:
+        print(f"- {item['title']}")
+
+if __name__ == "__main__":
+    run()
